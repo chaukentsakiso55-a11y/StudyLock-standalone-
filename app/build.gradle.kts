41004b395850ea386ac1989066bf277ac0f593a9
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-03 17:18:47 +0200

    Use Kotlin 2.3 compiler options DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,16 +76,18 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         buildConfig = true
     }
 
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
